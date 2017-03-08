f57b557833c255d3e9a17c1cec55b1efc4782251
Jajati Keshari Routray <jajatikr@gmail.com>
2017-03-07 19:26:38 -0700

    Working rough code

--- download_middlebury_stereo_dataset.py
+++ download_middlebury_stereo_dataset.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Author: Jajati Keshari Routray
+# Date Created: Mar 7 2017
+# Usage: Download Middlebury stereo dataset
+# Requirements: BeautifulSoup, lxml library
+# License: MIT
+# <https://github.com/jajatikr/download-middlebury-stereo-dataset/blob/master/LICENSE>
+
+# TODO:
+# 1. File handling in Windows system, Passing filenames as arguments
+# 2. PEP8 formatting
+# 3. File download error handling, File already present handling
+# 4. File download progressbar
 
 import os
 import urllib
@@ -15,12 +29,14 @@ if not os.path.exists(dataset_folder):
 # Change current working directory to Dataset directory
 os.chdir(dataset_folder)
 
-source = urllib.request.urlopen('http://vision.middlebury.edu/stereo/data/scenes2014/zip/')
-# testfile.retrieve("http://vision.middlebury.edu/stereo/data/scenes2014/zip/Adirondack-imperfect.zip", "Adirondack-imperfect.zip")
+# Read and parse website html
+url = 'http://vision.middlebury.edu/stereo/data/scenes2014/zip/'
+source = urllib.request.urlopen(url)
 soup = BeautifulSoup(source, "lxml")
 
-# for i in soup.findAll('.zip', attrs={'src': re.compile('(?i)(jpg|png)$')}):
-#     full_url = urlparse.urljoin(url, i['src'])
-#     print "image URL: ", full_url
-
-# print(soup.prettify())
+# [<a href="xyz.zip">xyz.zip</a>, ...] anchor elements
+for link in soup.findAll('a'):
+    link_ext = link.get('href')  # xyz.zip
+    if link_ext.endswith('.zip'):
+        print('Downloading: ' + link_ext)
+        urllib.request.urlretrieve(url + link_ext, link_ext) # retrieve(url, filename)
