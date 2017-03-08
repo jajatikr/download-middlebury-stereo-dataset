f2678a094d94a50c35e1ac181906ae3db5bb8346
Jajati Keshari Routray <jajatikr@gmail.com>
2017-03-08 01:40:41 -0700

    Added download progress numbers

--- download_middlebury_stereo_dataset.py
+++ download_middlebury_stereo_dataset.py
@@ -13,8 +13,10 @@
 # 2. PEP8 formatting
 # 3. File download error handling, File already present handling
 # 4. File download progressbar
+# 5. Use regex in bs4 findAll instead of for loop to remove non-zip elements
 
 import os
+import sys
 import urllib
 from bs4 import BeautifulSoup
 
@@ -35,8 +37,21 @@ source = urllib.request.urlopen(url)
 soup = BeautifulSoup(source, "lxml")
 
 # [<a href="xyz.zip">xyz.zip</a>, ...] anchor elements
-for link in soup.findAll('a'):
-    link_ext = link.get('href')  # xyz.zip
+anchor_list = soup.findAll('a')
+zip_anchor_list = []
+
+# Remove non-zip anchor elements
+for link in anchor_list[:]:
+    link_ext = link.get('href')
     if link_ext.endswith('.zip'):
-        print('Downloading: ' + link_ext)
-        urllib.request.urlretrieve(url + link_ext, link_ext) # retrieve(url, filename)
+        zip_anchor_list.append(link_ext)
+
+count = 0
+print('Downloading...')
+
+for link_ext in zip_anchor_list:
+        count += 1
+        print('{}/{} : {}'.format(count, len(zip_anchor_list), link_ext))
+        urllib.request.urlretrieve(url + link_ext, link_ext)  # retrieve(url, filename)
+
+print('Download complete')
